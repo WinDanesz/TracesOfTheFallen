c740fdfa2a9dd07ce35b6679b56e807ff6dcb223
WinDanesz <31292708+windanesz@users.noreply.github.com>
2025-12-11 01:28:54 +0100

    Fix paintings

--- src/main/java/com/windanesz/tracesofthefallen/item/ItemModPainting.java
+++ src/main/java/com/windanesz/tracesofthefallen/item/ItemModPainting.java
@@ -76,7 +76,7 @@ public class ItemModPainting extends Item {
 					NBTTagCompound ownerTag = itemstack.getSubCompound("Owner");
 					if (ownerTag != null && ownerTag.hasKey("UUID", 8)) {
 						painting.setOwnerId(java.util.UUID.fromString(ownerTag.getString("UUID")));
-						painting.setOwnerName(player.getName());
+						painting.setOwnerName(ownerTag.getString("PlayerName"));
 					} else {
 						painting.setOwnerId(player.getUniqueID());
 						painting.setOwnerName(player.getName());
